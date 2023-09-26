4cecd9d75638318ff7f21726dc27340ed816babf
taufeeque9 <9taufeeque9@gmail.com>
2023-09-26 18:12:35 +0530

    Add util and webapp files

---
+++ codebook_features/code_search_cache.py
@@ -0,0 +1,212 @@
+"""Script to generate cache for code search webapp."""
+
+import argparse
+import glob
+import multiprocessing as mp
+import os
+import pickle
+from datetime import datetime
+from functools import partial
+from typing import Dict
+
+import numpy as np
+import transformers
+
+from codebook_features import models, run_clm, utils
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--model_name", type=str, required=True)
+parser.add_argument("--pretrained_path", type=str, required=True)
+parser.add_argument("--max_samples", type=int, default=2000)
+parser.add_argument("--seed", type=int, default=42)
+parser.add_argument("--output_base_dir", type=str, default="/shared/cb_eval_acts/")
+parser.add_argument("--regen_cache", default=False, action="store_true")
+parser.add_argument("--dataset_name", type=str, default=None)
+parser.add_argument("--dataset_config_name", type=str, default=None)
+
+
+args = parser.parse_args()
+model_name_or_path = args.model_name
+model_id = model_name_or_path.split("/")[-1]
+pretrained_path = args.pretrained_path
+max_samples = args.max_samples
+seed = args.seed
+base_dir = args.output_base_dir
+regen_cache = args.regen_cache
+dataset_name = args.dataset_name
+dataset_config_name = args.dataset_config_name
+
+device = "cuda"
+orig_cb_model = models.wrap_codebook(
+    model_or_path=model_name_or_path, pretrained_path=pretrained_path
+)
+orig_cb_model = orig_cb_model.to(device).eval()
+orig_cb_model.disable_logging()
+
+assert (
+    len(orig_cb_model.config.codebook_type) == 1
+), "Multi-codebook per layer not supported."
+
+cb_at_dict = {
+    "preproj_attention": "attn_preproj",
+    "transformer_block": "tb",
+    "attention": "attn",
+    "attention_and_mlp": "attn+mlp",
+}
+
+cb_at = orig_cb_model.config.codebook_at[0]
+cb_at = cb_at_dict.get(cb_at, cb_at)
+is_attn = "attn" in cb_at
+ccb = orig_cb_model.config.codebook_type[0] == "group"
+n_layers = orig_cb_model.num_layers()
+n_heads = orig_cb_model.config.num_codebooks[0] if is_attn else None
+num_codes = orig_cb_model.config.num_codes
+tokenizer = transformers.AutoTokenizer.from_pretrained(pretrained_path)
+
+print("Loaded original cb model.")
+
+report_to = "none"
+training_args = run_clm.TrainingArguments(
+    output_dir=f"/shared/output_{cb_at}_{model_id}/",
+    do_eval=True,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=32,
+)
+
+model_args = run_clm.ModelArguments(
+    model_name_or_path=pretrained_path, cache_dir="/shared/.cache/huggingface/"
+)
+if not dataset_name:
+    if "tinystories" in model_id.lower():
+        dataset_name = "roneneldan/TinyStories"
+        data_args = run_clm.DataTrainingArguments(
+            dataset_name=dataset_name,
+            dataset_config_name=None,
+            streaming=False,
+            block_size=512,
+        )
+    else:
+        dataset_name = "wikitext"
+        data_args = run_clm.DataTrainingArguments(
+            dataset_name=dataset_name,
+            dataset_config_name="wikitext-103-v1",
+            streaming=False,
+        )
+else:  # used when tinystories is trained on wikitext for example
+    assert dataset_config_name is not None
+    data_args = run_clm.DataTrainingArguments(
+        dataset_name=dataset_name,
+        dataset_config_name=dataset_config_name,
+        streaming=False,
+    )
+
+trainer, lm_datasets, raw_datasets, last_checkpoint = run_clm.get_trainer_and_dataset(
+    model_args,
+    data_args,
+    training_args,
+    orig_cb_model,
+    optimizers=(None, None),
+)
+
+rng = np.random.default_rng(seed)
+indices = rng.choice(len(lm_datasets["train"]), max_samples, replace=False)
+dataset = lm_datasets["train"].select(indices)
+tokens = dataset["input_ids"]
+tokens = np.array(tokens)
+tokens_text = tokenizer.batch_decode(tokens, clean_up_tokenization_spaces=False)
+
+cb_acts: Dict[str, np.ndarray] = {}
+
+
+def store_cb_activations(key, codebook_ids, codebook_acts=cb_acts):
+    """Store activations for a given codebook."""
+    assert len(codebook_ids.shape) == 3  # (bs, seq_len, k_codebook)
+    if key not in codebook_acts:
+        codebook_acts[key] = []
+    codebook_acts[key].append(codebook_ids)
+
+
+output_dir = base_dir + f"{model_id}_{'c' if ccb else 'v'}cb_{cb_at}*"
+dirs = glob.glob(output_dir)
+dirs.sort(key=os.path.getmtime)
+if len(dirs) > 0 and (not regen_cache):
+    output_dir = dirs[-1]
+    print("Loading activations from", output_dir)
+    with open(f"{output_dir}/cb_acts.pkl", "rb") as f:
+        cb_acts = pickle.load(f)
+    tokens = np.load(f"{output_dir}/tokens.npy")
+    metrics = np.load(f"{output_dir}/metrics.npy", allow_pickle=True)
+else:
+    orig_cb_model.set_hook_fn(store_cb_activations)
+    orig_cb_model.reset_codebook_metrics(), orig_cb_model.reset_hook_kwargs()
+    orig_cb_model.enable_logging()
+    metrics = trainer.evaluate(dataset)
+    for k, v in cb_acts.items():
+        cb_acts[k] = np.concatenate(v, axis=0)
+    output_dir = output_dir[:-1] + "_" + datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    os.makedirs(output_dir, exist_ok=True)
+    np.save(f"{output_dir}/tokens.npy", tokens)
+    np.save(f"{output_dir}/metrics.npy", metrics)
+    with open(f"{output_dir}/cb_acts.pkl", "wb") as f:  # type: ignore
+        pickle.dump(cb_acts, f)
+    print("Saved activations to", output_dir)
+
+print(metrics)
+
+tokens_str = [
+    tokenizer.convert_ids_to_tokens(tokens[i]) for i in range(tokens.shape[0])
+]
+tokens_str = [
+    [tokenizer.convert_tokens_to_string([t]) for t in t_list] for t_list in tokens_str
+]
+tokens_str_len_arr = np.array([[len(t) for t in t_list] for t_list in tokens_str])
+token_byte_pos = np.cumsum(tokens_str_len_arr, axis=1)
+
+
+def get_act_counts_for_comp(layer=None, head=None):
+    """Get the number of activations for each code in the codebook at `layer` and `head`."""
+    comp_name = f"layer{layer}_{cb_at}{f'_ccb{head}' if ccb else ''}"
+    ft_tkns = utils.features_to_tokens(comp_name, cb_acts, num_codes=num_codes)
+    acts_count = np.array([len(v) for v in ft_tkns])
+    return f"layer{layer}{f'_head{head}' if ccb else ''}", acts_count
+
+
+act_count_ft_tkns = {}
+if is_attn:
+    with mp.Pool(min(16, n_heads)) as pool:
+        for layer in range(0, n_layers):
+            process_component_partial = partial(get_act_counts_for_comp, layer)
+            for comp_name, acts_count in pool.map(
+                process_component_partial, range(0, n_heads)
+            ):
+                act_count_ft_tkns[comp_name] = acts_count
+else:
+    with mp.Pool(min(16, n_layers)) as pool:
+        for comp_name, acts_count in pool.map(
+            get_act_counts_for_comp, range(0, n_layers)
+        ):
+            act_count_ft_tkns[comp_name] = acts_count
+
+# save all the data
+np.save(output_dir + "/tokens.npy", tokens)
+np.save(output_dir + "/tokens_str.npy", tokens_str)
+np.save(output_dir + "/tokens_text.npy", tokens_text)
+np.save(output_dir + "/token_byte_pos.npy", token_byte_pos)
+with open(output_dir + "/act_count_ft_tkns.pkl", "wb") as f:  # type: ignore
+    pickle.dump(act_count_ft_tkns, f)
+
+model_info = utils.ModelInfoForWebapp(
+    model_name=model_id,
+    pretrained_path=pretrained_path,
+    dataset_name=dataset_name,
+    num_codes=num_codes,
+    cb_at=cb_at,
+    ccb=ccb,
+    n_layers=n_layers,
+    n_heads=n_heads,
+    seed=seed,
+    max_samples=max_samples,
+)
+model_info.save(output_dir + "/info.txt")
+print("Saved all data to", output_dir)
+print("Saved all data to", output_dir)
---
+++ codebook_features/utils.py
@@ -0,0 +1,563 @@
+"""Util functions for codebook features."""
+
+import pathlib
+import re
+import typing
+from dataclasses import dataclass
+from functools import partial
+from typing import Optional
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+from termcolor import colored
+from tqdm import tqdm
+
+
+@dataclass
+class CodeInfo:
+    """Dataclass for codebook info."""
+
+    code: int
+    layer: int
+    head: Optional[int]
+    cb_at: Optional[str] = None
+
+    # for patching interventions
+    pos: Optional[int] = None
+    code_pos: Optional[int] = -1
+
+    # for description & regex-based interpretation
+    description: Optional[str] = None
+    regex: Optional[str] = None
+    prec: Optional[float] = None
+    recall: Optional[float] = None
+    num_acts: Optional[int] = None
+
+    def __post_init__(self):
+        """Convert to appropriate types."""
+        self.code = int(self.code)
+        self.layer = int(self.layer)
+        if self.head:
+            self.head = int(self.head)
+        if self.pos:
+            self.pos = int(self.pos)
+        if self.code_pos:
+            self.code_pos = int(self.code_pos)
+        if self.prec:
+            self.prec = float(self.prec)
+            assert 0 <= self.prec <= 1
+        if self.recall:
+            self.recall = float(self.recall)
+            assert 0 <= self.recall <= 1
+        if self.num_acts:
+            self.num_acts = int(self.num_acts)
+
+    def check_description_info(self):
+        """Check if the regex info is present."""
+        assert self.num_acts is not None and self.description is not None
+        if self.regex is not None:
+            assert self.prec is not None and self.recall is not None
+
+    def __repr__(self):
+        """Return the string representation."""
+        repr = f"CodeInfo(code={self.code}, layer={self.layer}, head={self.head}, cb_at={self.cb_at}"
+        if self.pos is not None or self.code_pos is not None:
+            repr += f", pos={self.pos}, code_pos={self.code_pos}"
+        if self.description is not None:
+            repr += f", description={self.description}"
+        if self.regex is not None:
+            repr += f", regex={self.regex}, prec={self.prec}, recall={self.recall}"
+        if self.num_acts is not None:
+            repr += f", num_acts={self.num_acts}"
+        repr += ")"
+        return repr
+
+    @classmethod
+    def from_str(cls, code_txt, *args, **kwargs):
+        """Extract code info fields from string."""
+        code_txt = code_txt.strip()
+        code_txt = code_txt.split(", ")
+        code_txt = dict(txt.split(": ") for txt in code_txt)
+        return cls(*args, **code_txt, **kwargs)
+
+
+@dataclass
+class ModelInfoForWebapp:
+    """Model info for webapp."""
+
+    model_name: str
+    pretrained_path: str
+    dataset_name: str
+    num_codes: int
+    cb_at: str
+    ccb: str
+    n_layers: int
+    n_heads: Optional[int] = None
+    seed: int = 42
+    max_samples: int = 2000
+
+    def __post_init__(self):
+        """Convert to correct types."""
+        self.num_codes = int(self.num_codes)
+        self.n_layers = int(self.n_layers)
+        if self.n_heads == "None":
+            self.n_heads = None
+        elif self.n_heads is not None:
+            self.n_heads = int(self.n_heads)
+        self.seed = int(self.seed)
+        self.max_samples = int(self.max_samples)
+
+    @classmethod
+    def load(cls, path):
+        """Parse model info from path."""
+        path = pathlib.Path(path)
+        with open(path / "info.txt", "r") as f:
+            lines = f.readlines()
+            lines = dict(line.strip().split(": ") for line in lines)
+        return cls(**lines)
+
+    def save(self, path):
+        """Save model info to path."""
+        path = pathlib.Path(path)
+        with open(path / "info.txt", "w") as f:
+            for k, v in self.__dict__.items():
+                f.write(f"{k}: {v}\n")
+
+
+def logits_to_pred(logits, tokenizer, k=5):
+    """Convert logits to top-k predictions."""
+    sorted_logits, sorted_indices = torch.sort(logits, descending=True, dim=-1)
+    probs = sorted_logits.softmax(dim=-1)
+    topk_preds = [tokenizer.convert_ids_to_tokens(e) for e in sorted_indices[:, -1, :k]]
+    topk_preds = [
+        tokenizer.convert_tokens_to_string([e]) for batch in topk_preds for e in batch
+    ]
+    return [(topk_preds[i], probs[:, -1, i].item()) for i in range(len(topk_preds))]
+
+
+def features_to_tokens(cb_key, cb_acts, num_codes, code=None):
+    """Return the set of token ids each codebook feature activates on."""
+    codebook_ids = cb_acts[cb_key]
+
+    if code is None:
+        features_tokens = [[] for _ in range(num_codes)]
+        for i in tqdm(range(codebook_ids.shape[0])):
+            for j in range(codebook_ids.shape[1]):
+                for k in range(codebook_ids.shape[2]):
+                    features_tokens[codebook_ids[i, j, k]].append((i, j))
+    else:
+        idx0, idx1, _ = np.where(codebook_ids == code)
+        features_tokens = list(zip(idx0, idx1))
+
+    return features_tokens
+
+
+def color_str(s: str, html: bool, color: Optional[str] = None):
+    """Color the string for html or terminal."""
+    if html:
+        color = "DeepSkyBlue" if color is None else color
+        return f"<span style='color:{color}'>{s}</span>"
+    else:
+        color = "light_cyan" if color is None else color
+        return colored(s, color)
+
+
+def color_tokens_automata(tokens, color_idx, html=False):
+    """Separate states with a dash and color red the tokens in color_idx."""
+    ret_string = ""
+    itr_over_color_idx = 0
+    tokens_enumerate = enumerate(tokens)
+    if tokens[0] == "<|endoftext|>":
+        next(tokens_enumerate)
+        if color_idx[0] == 0:
+            itr_over_color_idx += 1
+    for i, c in tokens_enumerate:
+        if i % 2 == 1:
+            ret_string += "-"
+        if itr_over_color_idx < len(color_idx) and i == color_idx[itr_over_color_idx]:
+            ret_string += color_str(c, html)
+            itr_over_color_idx += 1
+        else:
+            ret_string += c
+    return ret_string
+
+
+def color_tokens(tokens, color_idx, n=3, html=False):
+    """Color the tokens in color_idx."""
+    ret_string = ""
+    last_colored_token_idx = -1
+    for i in color_idx:
+        c_str = tokens[i]
+        if i <= last_colored_token_idx + 2 * n + 1:
+            ret_string += "".join(tokens[last_colored_token_idx + 1 : i])
+        else:
+            ret_string += "".join(
+                tokens[last_colored_token_idx + 1 : last_colored_token_idx + n + 1]
+            )
+            ret_string += " ... "
+            ret_string += "".join(tokens[i - n : i])
+        ret_string += color_str(c_str, html)
+        last_colored_token_idx = i
+    ret_string += "".join(
+        tokens[
+            last_colored_token_idx + 1 : min(last_colored_token_idx + n, len(tokens))
+        ]
+    )
+    return ret_string
+
+
+def prepare_example_print(
+    example_id,
+    example_tokens,
+    tokens_to_color,
+    html,
+    color_fn=color_tokens,
+):
+    """Format example to print."""
+    example_output = color_str(example_id, html, "green")
+    example_output += (
+        ": "
+        + color_fn(example_tokens, tokens_to_color, html=html)
+        + ("<br>" if html else "\n")
+    )
+    return example_output
+
+
+def tkn_print(
+    ll,
+    tokens,
+    is_automata=False,
+    n=3,
+    max_examples=100,
+    randomize=False,
+    html=False,
+    return_example_list=False,
+):
+    """Format and prints the tokens in ll."""
+    if randomize:
+        raise NotImplementedError("Randomize not yet implemented.")
+    indices = range(len(ll))
+    print_output = [] if return_example_list else ""
+    curr_ex = ll[0][0]
+    total_examples = 0
+    tokens_to_color = []
+    color_fn = color_tokens_automata if is_automata else partial(color_tokens, n=n)
+    for idx in indices:
+        if total_examples > max_examples:
+            break
+        i, j = ll[idx]
+
+        if i != curr_ex and curr_ex >= 0:
+            curr_ex_output = prepare_example_print(
+                curr_ex,
+                tokens[curr_ex],
+                tokens_to_color,
+                html,
+                color_fn,
+            )
+            total_examples += 1
+            if return_example_list:
+                print_output.append((curr_ex_output, len(tokens_to_color)))
+            else:
+                print_output += curr_ex_output
+            curr_ex = i
+            tokens_to_color = []
+        tokens_to_color.append(j)
+    curr_ex_output = prepare_example_print(
+        curr_ex,
+        tokens[curr_ex],
+        tokens_to_color,
+        html,
+        color_fn,
+    )
+    if return_example_list:
+        print_output.append((curr_ex_output, len(tokens_to_color)))
+    else:
+        print_output += curr_ex_output
+        print_output += color_str("*" * 50, html, "green")
+    total_examples += 1
+
+    return print_output
+
+
+def print_ft_tkns(
+    ft_tkns,
+    tokens,
+    is_automata=False,
+    n=3,
+    start=0,
+    stop=1000,
+    indices=None,
+    max_examples=100,
+    freq_filter=None,
+    randomize=False,
+    html=False,
+    return_example_list=False,
+):
+    """Print the tokens for the codebook features."""
+    indices = list(range(start, stop)) if indices is None else indices
+    num_tokens = len(tokens) * len(tokens[0])
+    codes, token_act_freqs, token_acts = [], [], []
+    for i in indices:
+        tkns = ft_tkns[i]
+        freq = (len(tkns), 100 * len(tkns) / num_tokens)
+        if freq_filter is not None and freq[1] > freq_filter:
+            continue
+        codes.append(i)
+        token_act_freqs.append(freq)
+        if len(tkns) > 0:
+            tkn_acts = tkn_print(
+                tkns,
+                tokens,
+                is_automata,
+                n=n,
+                max_examples=max_examples,
+                randomize=randomize,
+                html=html,
+                return_example_list=return_example_list,
+            )
+            token_acts.append(tkn_acts)
+        else:
+            token_acts.append("")
+    return codes, token_act_freqs, token_acts
+
+
+def patch_in_codes(run_cb_ids, hook, pos, code, code_pos=None):
+    """Patch in the `code` at `run_cb_ids`."""
+    pos = slice(None) if pos is None else pos
+    code_pos = slice(None) if code_pos is None else code_pos
+
+    if code_pos == "append":
+        assert pos == slice(None)
+        run_cb_ids = F.pad(run_cb_ids, (0, 1), mode="constant", value=code)
+    if isinstance(pos, typing.Iterable) or isinstance(pos, typing.Iterable):
+        for p in pos:
+            run_cb_ids[:, p, code_pos] = code
+    else:
+        run_cb_ids[:, pos, code_pos] = code
+    return run_cb_ids
+
+
+def get_cb_layer_name(cb_at, layer_idx, head_idx=None):
+    """Get the layer name used to store hooks/cache."""
+    if head_idx is None:
+        return f"blocks.{layer_idx}.{cb_at}.codebook_layer.hook_codebook_ids"
+    else:
+        return f"blocks.{layer_idx}.{cb_at}.codebook_layer.codebook.{head_idx}.hook_codebook_ids"
+
+
+def get_cb_layer_names(layer, patch_types, n_heads):
+    """Get the layer names used to store hooks/cache."""
+    layer_names = []
+    attn_added, mlp_added = False, False
+    if "attn_out" in patch_types:
+        attn_added = True
+        for head in range(n_heads):
+            layer_names.append(
+                f"blocks.{layer}.attn.codebook_layer.codebook.{head}.hook_codebook_ids"
+            )
+    if "mlp_out" in patch_types:
+        mlp_added = True
+        layer_names.append(f"blocks.{layer}.mlp.codebook_layer.hook_codebook_ids")
+
+    for patch_type in patch_types:
+        # match patch_type of the pattern attn_\d_head_\d
+        attn_head = re.match(r"attn_(\d)_head_(\d)", patch_type)
+        if (not attn_added) and attn_head and attn_head[1] == str(layer):
+            layer_names.append(
+                f"blocks.{layer}.attn.codebook_layer.codebook.{attn_head[2]}.hook_codebook_ids"
+            )
+        mlp = re.match(r"mlp_(\d)", patch_type)
+        if (not mlp_added) and mlp and mlp[1] == str(layer):
+            layer_names.append(f"blocks.{layer}.mlp.codebook_layer.hook_codebook_ids")
+
+    return layer_names
+
+
+def cb_layer_name_to_info(layer_name):
+    """Get the layer info from the layer name."""
+    layer_name_split = layer_name.split(".")
+    layer_idx = int(layer_name_split[1])
+    cb_at = layer_name_split[2]
+    if cb_at == "mlp":
+        head_idx = None
+    else:
+        head_idx = int(layer_name_split[5])
+    return cb_at, layer_idx, head_idx
+
+
+def run_with_codes(
+    input,
+    cb_model,
+    code,
+    cb_at,
+    layer_idx,
+    head_idx=None,
+    pos=None,
+    code_pos=None,
+    prepend_bos=True,
+):
+    """Run the model with the codebook features patched in."""
+    hook_fns = [
+        partial(patch_in_codes, pos=pos, code=code[i], code_pos=code_pos)
+        for i in range(len(code))
+    ]
+    cb_model.reset_codebook_metrics()
+    cb_model.reset_hook_kwargs()
+    fwd_hooks = [
+        (get_cb_layer_name(cb_at[i], layer_idx[i], head_idx[i]), hook_fns[i])
+        for i in range(len(cb_at))
+    ]
+    with cb_model.hooks(fwd_hooks, [], True, False) as hooked_model:
+        patched_logits, patched_cache = hooked_model.run_with_cache(
+            input, prepend_bos=prepend_bos
+        )
+    return patched_logits, patched_cache
+
+
+def in_hook_list(list_of_arg_tuples, layer, head=None):
+    """Check if the component specified by `layer` and `head` is in the `list_of_arg_tuples`."""
+    # if head is not provided, then checks in MLP
+    for arg_tuple in list_of_arg_tuples:
+        if head is None:
+            if arg_tuple.cb_at == "mlp" and arg_tuple.layer == layer:
+                return True
+        else:
+            if (
+                arg_tuple.cb_at == "attn"
+                and arg_tuple.layer == layer
+                and arg_tuple.head == head
+            ):
+                return True
+    return False
+
+
+def generate_with_codes(
+    input,
+    cb_model,
+    list_of_code_infos=(),
+    disable_other_comps=False,
+    automata=None,
+    generate_kwargs=None,
+):
+    """Model's generation with the codebook features patched in."""
+    if generate_kwargs is None:
+        generate_kwargs = {}
+    hook_fns = [
+        partial(patch_in_codes, pos=tupl.pos, code=tupl.code, code_pos=tupl.code_pos)
+        for tupl in list_of_code_infos
+    ]
+    fwd_hooks = [
+        (get_cb_layer_name(tupl.cb_at, tupl.layer, tupl.head), hook_fns[i])
+        for i, tupl in enumerate(list_of_code_infos)
+    ]
+    cb_model.reset_hook_kwargs()
+    if disable_other_comps:
+        for layer, cb in cb_model.all_codebooks.items():
+            for head_idx, head in enumerate(cb[0].codebook):
+                if not in_hook_list(list_of_code_infos, layer, head_idx):
+                    head.set_hook_kwargs(
+                        disable_topk=1, disable_for_tkns=[-1], keep_k_codes=False
+                    )
+            if not in_hook_list(list_of_code_infos, layer):
+                cb[1].set_hook_kwargs(
+                    disable_topk=1, disable_for_tkns=[-1], keep_k_codes=False
+                )
+    with cb_model.hooks(fwd_hooks, [], True, False) as hooked_model:
+        gen = hooked_model.generate(input, **generate_kwargs)
+    return automata.seq_to_traj(gen)[0] if automata is not None else gen
+
+
+def kl_div(logits1, logits2, pos=-1, reduction="batchmean"):
+    """Calculate the KL divergence between the logits at `pos`."""
+    logits1_last, logits2_last = logits1[:, pos, :], logits2[:, pos, :]
+    return F.kl_div(
+        F.log_softmax(logits1_last, dim=-1),
+        F.log_softmax(logits2_last, dim=-1),
+        log_target=True,
+        reduction=reduction,
+    )
+
+
+def JSD(logits1, logits2, pos=-1, reduction="batchmean"):
+    """Compute the Jensen-Shannon divergence between two distributions."""
+    if len(logits1.shape) == 3:
+        logits1, logits2 = logits1[:, pos, :], logits2[:, pos, :]
+
+    probs1 = F.softmax(logits1, dim=-1)
+    probs2 = F.softmax(logits2, dim=-1)
+
+    total_m = (0.5 * (probs1 + probs2)).log()
+
+    loss = 0.0
+    loss += F.kl_div(
+        total_m,
+        F.log_softmax(logits1, dim=-1),
+        log_target=True,
+        reduction=reduction,
+    )
+    loss += F.kl_div(
+        total_m,
+        F.log_softmax(logits2, dim=-1),
+        log_target=True,
+        reduction=reduction,
+    )
+    return 0.5 * loss
+
+
+def residual_stream_patching_hook(resid_pre, hook, cache, position: int):
+    """Patch in the codebook features at `position` from `cache`."""
+    clean_resid_pre = cache[hook.name]
+    resid_pre[:, position, :] = clean_resid_pre[:, position, :]
+    return resid_pre
+
+
+def find_code_changes(cache1, cache2, pos=None):
+    """Find the codebook codes that are different between the two caches."""
+    for k in cache1.keys():
+        if "codebook" in k:
+            c1 = cache1[k][0, pos]
+            c2 = cache2[k][0, pos]
+            if not torch.all(c1 == c2):
+                print(cb_layer_name_to_info(k), c1.tolist(), c2.tolist())
+                print(cb_layer_name_to_info(k), c1.tolist(), c2.tolist())
+
+
+def common_codes_in_cache(cache_codes, threshold=0.0):
+    """Get the common code in the cache."""
+    codes, counts = torch.unique(cache_codes, return_counts=True, sorted=True)
+    counts = counts.float() * 100
+    counts /= cache_codes.shape[1]
+    counts, indices = torch.sort(counts, descending=True)
+    codes = codes[indices]
+    indices = counts > threshold
+    codes, counts = codes[indices], counts[indices]
+    return codes, counts
+
+
+def parse_topic_codes_string(
+    info_str: str,
+    pos: Optional[int] = None,
+    code_append: Optional[bool] = False,
+):
+    """Parse the topic codes string."""
+    code_info_strs = info_str.strip().split("\n")
+    code_info_strs = [e.strip() for e in code_info_strs if e]
+    topic_codes = []
+    layer, head = None, None
+    if code_append is None:
+        code_pos = None
+    else:
+        code_pos = "append" if code_append else -1
+    for code_info_str in code_info_strs:
+        topic_codes.append(CodeInfo.from_str(code_info_str, pos=pos, code_pos=code_pos))
+        if code_append is None or code_append:
+            continue
+        if layer == topic_codes[-1].layer and head == topic_codes[-1].head:
+            code_pos -= 1  # type: ignore
+        else:
+            code_pos = -1
+        topic_codes[-1].code_pos = code_pos
+        layer, head = topic_codes[-1].layer, topic_codes[-1].head
+    return topic_codes
